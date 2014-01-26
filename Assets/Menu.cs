8ed1235d257c185865070dc52a65a8b40224af2e
Peter Bartosch <bartoschp@gmail.com>
2014-01-26 07:49:22 -0600

    Forgot to add these files in prev commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public GUIStyle style;
+	void OnGUI(){
+		if(GUI.Button( new Rect(Screen.width/6,Screen.height/2-120,150,50),"MAIN MENU",style)){
+			Application.LoadLevel("title");
+		}
+		if(GUI.Button( new Rect(Screen.width/6,Screen.height/2-60,150,50),"Credits",style)){
+			//Application.LoadLevel(2);
+		}
+		if(GUI.Button( new Rect(Screen.width/6,Screen.height/2,150,50),"EXIT",style)){
+			Application.Quit();
+		}
+	}
+}
